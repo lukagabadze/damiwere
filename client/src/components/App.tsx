74699059e60c16b4bf10960d8731f04b8c835e62
lukagabadze <gabadzeluka2003@gmail.com>
2021-10-01 01:56:55 +0400

    tailwind color palette

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,5 +1,22 @@
 import { ReactElement } from "react";
 
 export default function App(): ReactElement | null {
-  return <div className="bg-red-200">zd gabo</div>;
+  return (
+    <section>
+      <div className="bg-main-100 ">zd gabo</div>
+      <div className="bg-main-200">zd gabo</div>
+      <div className="bg-main-300 text-white">zd gabo</div>
+      <div className="bg-main-400 text-white">zd gabo</div>
+      <div className="bg-main-500 text-white">zd gabo</div>
+
+      <div>-</div>
+      <div>-</div>
+
+      <div className="bg-secondary-500 text-white">zd gabo</div>
+      <div className="bg-secondary-400 text-white">zd gabo</div>
+      <div className="bg-secondary-300 text-white">zd gabo</div>
+      <div className="bg-secondary-200">zd gabo</div>
+      <div className="bg-secondary-100">zd gabo</div>
+    </section>
+  );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "main-100": "#038C4C",
+        "main-200": "#02733E",
+        main: "#01401C",
+        "main-300": "#01401C",
+        "main-400": "#012611",
+        "main-500": "#0D0D0D",
+        "secondary-100": "#F7F4D5",
+        "secondary-200": "#CBC5B5",
+        "secondary-300": "#32304A",
+        "secondary-400": "#1D1D39",
+        "secondary-500": "#110F29",
+      },
+    },
   },
   variants: {
     extend: {},
